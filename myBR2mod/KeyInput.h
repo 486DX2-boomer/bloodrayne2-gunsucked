0296886c45e354a5ce73f26b985dad116cdd9b72
Danny <danny@dfeller.xyz>
2025-12-26 13:38:51 -0500

    Ignore key inputs if game window not focused

--- myBR2mod/KeyInput.h
+++ myBR2mod/KeyInput.h
@@ -9,6 +9,23 @@ private:
     bool isToggleKey;  // true = fires once per press, false = fires while held
     std::function<void()> callback;  // Optional
 
+    // use to ignore input if rayne2.exe isn't active
+    // So this fixes keybinds working when the game window isn't focused,
+    // but it's still possible to trigger them when the game is paused.
+    // I guess that's fine for photo mode but it's weird to be able to switch weapons when paused
+    // it doesn't seem like super slow mo toggles when paused so the game's pause state isn't
+    // based on time factor; should be possible to find where the game tracks
+    // pause state and account for that too- if it's worth the effort
+    bool isGameFocused() {
+        HWND foreground = GetForegroundWindow();
+        if (foreground == nullptr) return false;
+
+        DWORD foregroundPid;
+        GetWindowThreadProcessId(foreground, &foregroundPid);
+
+        return foregroundPid == GetCurrentProcessId();
+    }
+
 public:
     // no callback
     KeyInput(int vkCode, bool toggle = true)
@@ -41,6 +58,11 @@ public:
     }
 
     void checkAndExecute() {
+
+        if (!this->isGameFocused()) { // ignore if window not active
+            return;
+        }
+
         if (this->isActivated() && this->callback != nullptr) {
             this->callback();
         }
